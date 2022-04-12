7a1b5c130ba0fa70579ec567179a2156c403881c
elencho <e.bochoradze@gmail.com>
2022-04-12 22:47:56 +0400

    feat: mapCard

---
+++ src/components/MapCard/MapCard.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+import { styles } from "./Styles";
+
+const MapCard = (props) => {
+  const { title, image } = props;
+  return (
+    <View style={styles.searchBox}>
+      
+      <Text>MapCard</Text>
+    </View>
+  );
+};
+
+export default MapCard;
