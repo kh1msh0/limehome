951af759653e9efdded840caf0ca37b9189706b0
elencho <e.bochoradze@gmail.com>
2022-04-13 00:11:04 +0400

    feat: marker done

--- src/components/MarkerMap/MarkerMap.jsx
+++ src/components/MarkerMap/MarkerMap.jsx
@@ -1,25 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
 import { Marker } from "react-native-maps";
 
 import { styles } from "./Styles";
 
 const MarkerMap = (props) => {
-  const { location, onPress, selected, latitude, longitude } = props;
+  const { onPress, selected, latitude, longitude, item } = props;
 
   const coordinate = { latitude: latitude, longitude: longitude };
   return (
-    <Marker
-      coordinate={coordinate}
-      pinColor={"purple"} // any color
-      title={"title"}
-      description={"description"}
-      style={styles.box}
-    >
-      <View style={styles.grayMarker}>
-        <Text style={styles.text}>10€</Text>
-      </View>
-      <View style={styles.arrow}/>
+    <Marker coordinate={coordinate} style={styles.box}>
+      <TouchableOpacity
+        style={selected ? styles.selected : styles.grayMarker}
+        activeOpacity={0.7}
+        onPress={() => onPress(item)}
+      >
+        <Text style={styles.text}>15€</Text>
+      </TouchableOpacity>
+      <View style={selected ? styles.arrowSelected : styles.arrow} />
     </Marker>
   );
 };
--- src/components/MarkerMap/Styles.jsx
+++ src/components/MarkerMap/Styles.jsx
@@ -3,7 +3,7 @@ import { StyleSheet, Dimensions, Platform } from "react-native";
 export const styles = StyleSheet.create({
   grayMarker: {
     backgroundColor: "#4A4B4D",
-    padding: 15,
+    padding: 12,
     borderRadius: 5,
   },
   text: {
@@ -19,5 +19,17 @@ export const styles = StyleSheet.create({
   },
   box: {
     alignItems: "center",
+  },
+  selected: {
+    backgroundColor: "#B16421",
+    padding: 12,
+    borderRadius: 10,
+  },
+  arrowSelected: {
+    height: 10,
+    marginTop: -5,
+    width: 10,
+    backgroundColor: "#B16421",
+    transform: [{ rotate: "45deg" }],
   },
 });
