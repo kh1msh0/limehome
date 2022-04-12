69d29c38410c8cf82a95a69904b1457a43a89122
elencho <e.bochoradze@gmail.com>
2022-04-12 22:47:13 +0400

    feat: module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "babel-plugin-module-resolver",
+        {
+          alias: {
+            assets: "./assets",
+            images: "./assets/images",
+            components: "./src/components",
+            screens: "./src/screens",
+            data: "./src/data",
+            utiles: "./src/utiles",
+          },
+        },
+      ],
+    ],
   };
 };
