348c9e0303a4244df0ee550e385f48023b8168fe
elencho <e.bochoradze@gmail.com>
2022-04-13 00:10:49 +0400

    feat: mapCard done

--- src/components/MapCard/MapCard.jsx
+++ src/components/MapCard/MapCard.jsx
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, ImageBackground } from "react-native";
 import React from "react";
+
+import { EvilIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 import { styles } from "./Styles";
 
 const MapCard = (props) => {
-  const { title, image } = props;
+  const { title, images, location, rating, priceFrom } = props;
+
   return (
     <View style={styles.searchBox}>
-      
-      <Text>MapCard</Text>
+      <ImageBackground source={{ uri: images[0]?.url }} style={styles.image}>
+        <View style={styles.imageWrapper}>
+          <Text style={styles.rating}>{rating} </Text>
+          <AntDesign name="star" size={24} color="#B16421" />
+        </View>
+      </ImageBackground>
+      <View style={styles.contentWrapper}>
+        <Text style={styles.title}>{title}</Text>
+        <View style={styles.locationWrapper}>
+          <EvilIcons name="location" size={30} color="#B16421" />
+          <Text style={styles.location}>{location}</Text>
+        </View>
+
+        <View style={styles.line} />
+        <Text style={styles.price}>
+          From <Text style={styles.priceHighlighted}>{priceFrom}</Text>
+        </Text>
+      </View>
     </View>
   );
 };
--- src/components/MapCard/Styles.jsx
+++ src/components/MapCard/Styles.jsx
@@ -1,30 +1,84 @@
-import {StyleSheet, Dimensions, Platform} from 'react-native';
+import { StyleSheet, Dimensions, Platform } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    map: {
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
-    },
-    searchBox: {
-      position:'absolute', 
-      marginTop: Platform.OS === 'ios' ? 40 : 20, 
-      flexDirection:"row",
-      backgroundColor: '#fff',
-      width: '90%',
-      alignSelf:'center',
-      borderRadius: 5,
-      padding: 10,
-      shadowColor: '#ccc',
-      shadowOffset: { width: 0, height: 3 },
-      shadowOpacity: 0.5,
-      shadowRadius: 5,
-      elevation: 10,
-      bottom: 5,
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  searchBox: {
+    position: "absolute",
+    marginTop: Platform.OS === "ios" ? 40 : 20,
+    flexDirection: "row",
+    backgroundColor: "#F7F1E9",
+    width: "90%",
+    alignSelf: "center",
+    borderRadius: 10,
+    shadowColor: "#ccc",
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 10,
+    bottom: 5,
+    flexDirection: "row",
+  },
+  image: {
+    width: 120,
+    height: 120,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#4A4B4D",
+  },
+  contentWrapper: {
+    marginLeft: 10,
+    marginTop: 10,
+    flex: 1,
+  },
+  locationWrapper: {
+    alignItems: "center",
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  location: {
+    fontSize: 16,
+    color: "#4A4B4D",
+    fontWeight: "300",
+  },
+  line: {
+    backgroundColor: "#4A4B4D",
+    height: 1,
+    marginRight: 10,
+    marginVertical: 10,
+  },
+  price: {
+    fontSize: 16,
+    color: "#4A4B4D",
+    fontWeight: "300",
+  },
+  priceHighlighted: {
+    fontSize: 16,
+    color: "#B16421",
+    fontWeight: "500",
+  },
+  imageWrapper: {
+    backgroundColor: "#F7F1E9",
+    flexDirection: "row",
+    alignSelf: "flex-end",
+    alignItems: "center",
+    padding: 3,
+    borderRadius: 5,
+    margin: 10
+  },
+  rating: {
+    fontSize: 15,
+    color: "#4A4B4D",
+    fontWeight: "400",
+  }
+});
